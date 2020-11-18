3be1ac43760a489a895cf8f08f2147a1a473f486
Alexandre <3979539+alxblog@users.noreply.github.com>
2020-11-19 00:48:01 +0100

    Fix stuff

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
-const packageJson = require('./package.json')
+'use strict';
+
+const packageJson = require('../package.json')
 const path = require('path')
 const fs = require('fs')
 const webpack = require('webpack')
